160e01855ee9c3c04bc4d78468c2fa79edd75e82
Tasmay-Holkar <tasmay.holkar@publicissapient.com>
2021-04-26 15:40:22 +0530

    Subtraction

--- BasicJava/MySecondClass.java
+++ BasicJava/MySecondClass.java
@@ -2,9 +2,10 @@ public class MySecondClass {
 
    public static void main(String[] args) {
         
-      int a = 5, b = 15, c;
+      int a = 5, b = 15, c, d;
       c = a + b;
+      d = a - b;
 
-      System.out.println("Sum is: "+c);
+      System.out.println("Diff is: "+d);
    }
 }
