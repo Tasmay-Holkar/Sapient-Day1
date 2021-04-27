e9e417bb8cd3264219678d43589d6ee0c19358c7
Tasmay-Holkar <tasmay.holkar@publicissapient.com>
2021-04-27 08:55:00 +0530

    Multiplication

--- BasicJava/MySecondClass.java
+++ BasicJava/MySecondClass.java
@@ -3,8 +3,8 @@ public class MySecondClass {
    public static void main(String[] args) {
         
       int a = 5, b = 15, c;
-      c = a + b;
+      c = a * b;
 
-      System.out.println("Sum is: "+c);
+      System.out.println("Multiplication is: "+c);
    }
 }
