25605192bb3fdb8bdd4de8359b8e70cedcafcd4a
Tasmay-Holkar <tasmay.holkar@publicissapient.com>
2021-04-27 14:43:39 +0530

    Maven

---
+++ day1/src/test/java/day1/MultiplyTwoNumbersTest.java
@@ -0,0 +1,14 @@
+package day1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MultiplyTwoNumbersTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
