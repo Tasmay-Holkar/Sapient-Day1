cac3c6c6bba9b5c865b882f9357afba8235d4d02
Tasmay-Holkar <tasmay.holkar@publicissapient.com>
2021-04-28 09:11:52 +0530

    Division

---
+++ BasicJava/Test2.java
@@ -0,0 +1,10 @@
+public class Test2 {
+
+   public static void main(String[] args) {
+        
+      int a = 15, b = 5, c;
+      c = a / b;
+
+      System.out.println("Division is: "+c);
+   }
+}
