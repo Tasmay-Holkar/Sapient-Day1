2e145de47a51d4dc23ff55eead9cdaf8f9383c50
Tasmay-Holkar <tasmay.holkar@publicissapient.com>
2021-04-28 09:06:36 +0530

    Subtraction

---
+++ BasicJava/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+
+   public static void main(String[] args) {
+        
+      int a = 15, b = 5, c;
+      c = a - b;
+
+      System.out.println("Subtraction is: "+c);
+   }
+}
